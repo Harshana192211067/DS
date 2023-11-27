567c8b4c1034cbc44bd1b9695978e99af840421a
Harshana192211067 <123436487+Harshana192211067@users.noreply.github.com>
2023-11-27 12:12:32 +0530

    Add files via upload

---
+++ exp_9 fibonaaci sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+ int n, a, b, next,sum=0;
+ printf("Enter the value of n: ");
+ scanf("%d", &n);
+ printf("Fibonacci series: ");
+ printf("0 1 "); 
+ a = 0;
+ b = 1;
+ next = a + b;
+ while (next <= n) {
+ printf("%d ", next);
+ a = b;
+ b = next;
+ next = a + b;
+ sum=sum+next;
+  }
+ printf("\n%d",sum);
+}
+
