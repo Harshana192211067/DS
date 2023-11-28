07a21560f70ec748272e75e1edbea4f91f2e1b77
Harshana192211067 <123436487+Harshana192211067@users.noreply.github.com>
2023-11-28 08:36:48 +0530

    Add files via upload

--- exp_27 sort alpha.c
+++ exp_27 sort alpha.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-
-int main() {
+int main() 
+{
     int i, j, n;
-
-    // Input the number of strings
     printf("Enter the number of strings: ");
     scanf("%d", &n);
 
@@ -12,16 +10,20 @@ int main() {
     char arr[n][100];  // Assuming a maximum string length of 100
 
     // Input the strings
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+	{
         printf("Enter string %d: ", i + 1);
         scanf("%s", arr[i]);
     }
 
     // Sort the array of strings using bubble sort
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (strcmp(arr[j], arr[j + 1]) > 0) {
-                // Swap the strings if they are in the wrong order
+    for (i = 0; i < n - 1; i++) 
+
+	{
+        for (j = 0; j < n - i - 1; j++) 
+		{
+            if (strcmp(arr[j], arr[j + 1]) > 0) 
+			{
                 char temp[100];
                 strcpy(temp, arr[j]);
                 strcpy(arr[j], arr[j + 1]);
@@ -29,13 +31,11 @@ int main() {
             }
         }
     }
-
-    // Display the sorted strings
     printf("\nStrings in alphabetical order:\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+	{
         printf("%s\n", arr[i]);
     }
-
     return 0;
 }
 
